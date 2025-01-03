e5d9a0d15e21594b7d154514a6d203397df3a9e8
“Harsh <harshwalia449@yahoo.com>
2025-01-03 23:08:39 +0530

    init and setup washmi and reac-query

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { config } from './config'
+
+const queryClient = new QueryClient()
+
+function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default App;
